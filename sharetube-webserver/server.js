95a817f9a8995e2ce53b5cb2d2e2b5ab943d3291
Belikov A <beli@sfedu.ru>
2023-07-18 19:27:57 +0300

    Project started

--- sharetube-webserver/server.js
+++ sharetube-webserver/server.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 81
+const fastify = require('fastify')({ logger: true })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+fastify.route({
+  method: 'GET',
+  url: '/api/upload',
+  schema: {
+    // the response needs to be an object with an `hello` property of type 'string'
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: { type: 'string' }
+        }
+      }
+    }
+  },
+  // this function is executed for every request before the handler is executed
+  preHandler: async (request, reply) => {
+    // E.g. check authentication
+  },
+  handler: async (request, reply) => {
+    return { hello: 'world' }
+  }
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+
+// Run the server!
+fastify.listen({ port: 81 }, (err) => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 })
